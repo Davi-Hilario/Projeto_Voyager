fb2cd8baf8c2506e750d814b1c6858b520310a3b
Davi-Hilario <davi.hilario@sptech.school>
2023-06-04 22:29:20 -0300

    banco com inserts testes

--- Voyager_DataBase/Script/Script_v.0.1.sql
+++ Voyager_DataBase/Script/Script_v.0.1.sql
@@ -12,24 +12,14 @@ create table if not exists Usuario
     senhaUsuario varchar(20) unique
 );
 
-insert into Usuario values
-	(default, 'Davi Alexandre', '2004-08-23', 'Saturno', 'Dia', 'davi.hilario@sptech.school','davi1234');
-insert into Usuario values
-	(default, 'Arnaldo Fritz', '1960-03-05', 'Mercúrio', 'Noite', 'arnaldoFritz@gmail.com','arnaldo1234'),
-	(default, 'Arthur Cervero', '1986-06-12', 'Saturno', 'Noite', 'arthurzin234@gmail.com','tutu1234'),
-	(default, 'Claudio Andrade', '2002-09-29', 'Jupiter', 'Dia', 'canedo@hotmail.com.br','caca1234'),
-	(default, 'Maria Madalena', '2006-04-01', 'Terra', 'Noite', 'maria@gmail.com','maria1234'),
-	(default, 'Leandra Almeida', '1976-05-11', 'Jupiter', 'Dia', 'lele@gmail.com','lele1234');
-    
-select * from Usuario;
-
 create table if not exists quiz
 (
 	idQuiz int primary key auto_increment,
     qtdAcertos int,
 	qtdErros int,
     fkUsuario int,
-    foreign key (fkUsuario) references Usuario(idUsuario)
+    foreign key (fkUsuario) references Usuario(idUsuario),
+    unique key (fkUsuario)
 );
 
 create table if not exists telefone
@@ -41,11 +31,48 @@ create table if not exists telefone
     foreign key(fkUsuario) references Usuario(idUsuario)
 );
 
-create unique index fkUser on quiz(fkUsuario);
+insert into Usuario values
+	(default, 'Arnaldo Fritz', '1960-03-05', 'Mercúrio', 'Noite', 'arnaldoFritz@gmail.com','arnaldo1234'),
+	(default, 'Arthur Cervero', '1986-06-12', 'Terra', 'Noite', 'arthurzin234@gmail.com','tutu1234'),
+	(default, 'Claudio Andrade', '2002-09-29', 'Terra', 'Dia', 'canedo@hotmail.com.br','caca1234'),
+	(default, 'Maria Madalena', '2006-04-01', 'Marte', 'Noite', 'maria@gmail.com','maria1234'),
+	(default, 'Leandra Almeida', '1976-05-11', 'Jupiter', 'Dia', 'lele@gmail.com','lele1234'),
+    (default, 'Rodolfo Carlos', '1961-04-02', 'Saturno', 'Noite', 'rodolfo@gmail.com','rodolfo1234'),
+	(default, 'Sidney Miguel', '1986-06-12', 'Saturno', 'Noite', 'sid@gmail.com','sidin1234'),
+	(default, 'Josoares Miranda', '1999-01-23', 'Netuno', 'Dia', 'josoares@hotmail.com.br','joso1234'),
+	(default, 'Maria Antonieta', '2003-03-03', 'Mercúrio', 'Noite', 'mariaAnt@gmail.com','mariaAnt1234'),
+	(default, 'Silvia Almeida', '1976-05-11', 'Saturno', 'Dia', 'silvia@gmail.com','silvia1234');
 
-select * from usuario;
-select * from telefone;
-select * from quiz;
+insert into Telefone values
+	(default, '11', '12345-6789', 1),
+	(default, '11', '24354-6239', 2),
+	(default, '11', '19343-1235', 3),
+	(default, '11', '58341-3842', 4),
+	(default, '11', '19068-1205', 5),
+	(default, '11', '94743-6509', 6),
+	(default, '11', '12994-4656', 7),
+	(default, '11', '12359-6449', 8),
+	(default, '11', '11234-6444', 9),
+	(default, '11', '19857-6938', 10);
+
+insert into Quiz values
+	(default, 4, 6, 1),
+	(default, 4, 6, 2),
+	(default, 5, 5, 3),
+	(default, 3, 7, 4),
+	(default, 10, 0, 5),
+	(default, 0, 10, 6),
+	(default, 6, 4, 7),
+	(default, 1, 9, 8),
+	(default, 9, 1, 9),
+	(default, 7, 3, 10);
+    
+select * from Usuario;
+select * from Telefone;
+select * from Quiz;
+
 select nomeUsuario, dataNasc, planetaFavorito, diaOuNoite, dddTel, numeroTel, qtdAcertos, qtdErros from usuario u 
 	join telefone t on u.idUsuario = t.fkUsuario
-    join quiz q on q.fkUsuario = u.idUsuario;
+	join quiz q on q.fkUsuario = u.idUsuario;
+
+delete from quiz where fkUsuario = 11;
